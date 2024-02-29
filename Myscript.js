ca09fa230943793ad7f9dae6a9bb74ae4e3d2392
Mikeisnotakid <teekaamichaele@gmail.com>
2024-02-29 16:47:53 +0100

    javascript

---
+++ Myscript.js
@@ -0,0 +1,33 @@
+var NewsletterInput = document.getElementById("newsletterinput");
+var NewsletterBtn = document.getElementById("newsletterbtn");
+var DisplayReaction = document.getElementById("alertnewsletter")
+
+NewsletterBtn.addEventListener("click", NewsletterBtnReact)
+
+
+function NewsletterBtnReact(event) {
+
+
+    if (NewsletterInput.value === "") {
+        DisplayReaction.innerHTML = "Please enter an email address"
+        DisplayReaction.style.color = "red"
+        DisplayReaction.style.backgroundColor = "white"
+        DisplayReaction.style.height = "50px"
+        
+
+
+    }
+
+
+    else {
+
+        DisplayReaction.innerHTML = "Succesfully Sent"
+        DisplayReaction.style.color = "blue"
+        NewsletterBtn.type = "submit";
+
+
+
+
+    }
+
+}
